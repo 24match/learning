2b067fa5878a91dc9d69ac4bcd146b7c2a30bb18
drew <liangpy@yeah.net>
2019-10-10 12:44:56 +0800

    netty 入门

---
+++ src/main/java/netty/TimeServer.java
@@ -0,0 +1,55 @@
+package netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @Author
+ * @Date 2019/10/10 11:10
+ * @Version
+ */
+public class TimeServer {
+    public static void main(String[] args) {
+        int port = 2424;
+        new TimeServer().bind(port);
+    }
+
+    /**
+     * 配置服务端的NIO线程池,用于网络事件处理,实际上他们是Reactor线程组
+     * bossGroup 用于服务端接受客户端连接,
+     * workerGroup 用于SocketChanel网络读写
+     * @param port
+     */
+    private void bind(int port) {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            //ServerBootstrap 是 Netty 用于启动 NIO 服务端的辅助启动类，用于降低开发难度
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG,1024)
+                    .childHandler(new ChildChannelHandler());
+
+            ChannelFuture f = serverBootstrap.bind(port).sync();
+
+            System.out.println(Thread.currentThread().getName()+",服务器开始监听端口,等待客户端连接.....");
+            //下面将进行阻塞
+            f.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel socketChannel) {
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+}
---
+++ src/main/java/netty/TimeServerHandler.java
@@ -0,0 +1,54 @@
+package netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * ChannelInboundHandlerAdapter extends ChannelHandlerAdapter 用于对网络事件进行读写操作
+ * @Author
+ * @Date 2019/10/10 11:21
+ * @Version
+ */
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext channelHandlerContext , Object msg) throws Exception {
+        /**
+         * 将 msg 转为 Netty 的 ByteBuf 对象，类似 JDK 中的 java.nio.ByteBuffer，不过 ButeBuf 功能更强，更灵活
+         */
+        ByteBuf buf = (ByteBuf) msg;
+        /**
+         * readableBytes：获取缓冲区可读字节数,然后创建字节数组
+         * 从而避免了像 java.nio.ByteBuffer 时，只能盲目的创建特定大小的字节数组，比如 1024
+         **/
+        byte[] reg = new byte[buf.readableBytes()];
+        String body = new String(reg,"UTF-8");
+        System.out.println(Thread.currentThread().getName() + ",The server receive order " + body);
+        /**
+         * readBytes：将缓冲区字节数组复制到新建的 byte 数组中
+         * 然后将字节数组转为字符串
+         **/
+        String respMsg = "I am Server，消息接收 success!";
+        ByteBuf respByteBuf = Unpooled.copiedBuffer(respMsg.getBytes());
+        channelHandlerContext.write(respByteBuf);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext channelHandlerContext){
+        /**
+         * flush：将消息发送队列中的消息写入到 SocketChannel 中发送给对方，为了频繁的唤醒 Selector 进行消息发送
+         * Netty 的 write 方法并不直接将消息写入 SocketChannel 中，调用 write 只是把待发送的消息放到发送缓存数组中，再通过调用 flush
+         * 方法，将发送缓冲区的消息全部写入到 SocketChannel 中
+         **/
+        channelHandlerContext.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext channelHandlerContext,Throwable cause) {
+        /** 当发生异常时，关闭 ChannelHandlerContext，释放和它相关联的句柄等资源 */
+        channelHandlerContext.close();
+    }
+
+}
